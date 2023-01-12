0b3a8c81af457ec3a45e2ffa358cf398f02d7dcb
SimoneMoima <69524243+SimoneMoima@users.noreply.github.com>
2023-01-12 10:08:46 +0100

    First commit

---
+++ weather-app/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+import "bootstrap/dist/css/bootstrap.css";
+import boostrap from "bootstrap/dist/js/bootstrap.bundle.js";
+
+const app = createApp(App);
+
+app.use(router).use(boostrap);
+
+app.mount("#app");
